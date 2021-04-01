66fec0c2bdffd6ff84a1f4fb234d30848997c175
xippoGmbH <36155028+xippoGmbH@users.noreply.github.com>
2021-04-01 18:33:58 +0200

    delete SRC Folder

---
+++ Command/GreetCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Xippogmbh\XippoErpCoreBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GreetCommand extends Command
+{
+    protected static $defaultName = 'xippo:generate';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Greet someone')
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+            ->addOption(
+                'yell',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will yell in uppercase letters'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : int
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+        return Command::SUCCESS;
+    }
+}
