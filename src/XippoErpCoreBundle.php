8fe220443fe0137a78ef37a0ee1d4d22d344d66c
xippoGmbH <aurelio.gisler@xippo.ch>
2021-04-08 18:24:54 +0200

    noting

--- src/XippoErpCoreBundle.php
+++ src/XippoErpCoreBundle.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Xippogmbh\XippoErpCoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Xippogmbh\XippoErpCoreBundle\DependencyInjection\XippoErpCoreExtension;
 
 class XippoErpCoreBundle extends Bundle
 {
-    
 }
