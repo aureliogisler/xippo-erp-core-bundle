cbe66e85db4d143bb694606354831c5bb2c6ce10
xippoGmbH <aurelio.gisler@xippo.ch>
2021-04-09 07:02:06 +0200

    first running route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,14 +4,12 @@ namespace Xippogmbh\XippoErpCoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('@xippo-erp-core/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
